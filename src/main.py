6deaa8202b820d99f59a5d630df4902035759e79
Carlos Vera A <carveraa13@gmail.com>
2021-04-08 03:00:18 +0000

    update

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Todo
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -20,24 +19,48 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/todos', methods=['GET'])
+def get_todo():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    todo_alls = Todo.query.all()
+    result = list(map(lambda x: x.serialize(), todo_alls))
 
-    return jsonify(response_body), 200
+    return jsonify(result), 200
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'label' not in body:
+        raise APIException('You need to specify the tasks', status_code=400)        
+
+    newTodo = Todo(task=body['label'], is_done=body['done'])
+    db.session.add(newTodo)
+    db.session.commit()
+    return "ok", 200
+
+@app.route('/todos/<int:id>', methods=['DELETE'])
+def remove_todo(id):
+
+    item = Todo.query.get(id)
+    if item is None:
+        raise APIException('Task not found', status_code=404)
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify("Task deleted successfully."), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
